4faec5514f5835ab1c19aaca81173af39b51ab95
giteshpandit <panditgitesh38@gmail.com>
2024-10-10 15:37:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-// complete this js code
-function Person(name, age) {}
+// Complete this js code
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
 
-function Employee(name, age, jobTitle) {}
+Person.prototype.greet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+};
+
+function Employee(name, age, jobTitle) {
+    // Inherit name and age from Person
+    Person.call(this, name, age);
+    this.jobTitle = jobTitle;
+}
+
+// Inherit the Person prototype in Employee
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
+
+Employee.prototype.jobGreet = function() {
+    console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
+};
 
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
+
